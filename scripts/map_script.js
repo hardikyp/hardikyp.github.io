4f0b49b17ca90ca8c6725b92762de87ef4d15805
hardikyp <hardikpatil4897@gmail.com>
2023-03-28 23:31:53 -0400

    a

--- scripts/map_script.js
+++ scripts/map_script.js
@@ -2,12 +2,12 @@ function initMap() {
     new google.maps.Map(document.getElementById("map"), {
       mapId: "2e1ff3edfa7ae7fb",
       center: { lat: 42.29336742303774, lng: -83.71556321232052 },
-      zoom: 17,
+      zoom: 17
     });
 
     new google.maps.Marker({
       position:{ lat: 42.29336742303774, lng: -83.71556321232052 },
       map,
-      title: "Hello world!",
+      animation: google.maps.Animation.DROP
     });
   }  
